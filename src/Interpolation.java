0e24dfbcfca63dd165534026755ee4963220616a
lipkinc18 <lipkinc18@students.ecu.edu>
2020-09-10 15:44:27 -0400

    Added local directory for images/ enabled frame keyListener (n,p,q)

--- src/Interpolation.java
+++ src/Interpolation.java
@@ -1,14 +1,16 @@
-import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -18,66 +20,81 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 public class Interpolation {
-	public static void main(String args[]) {
+	static ArrayList<Mat> mats = new ArrayList<Mat>();
+	static ArrayList<BufferedImage> images = new ArrayList<BufferedImage>();
+	static ArrayList<String> labelText = new ArrayList<String>();
+	static JFrame frame = new JFrame();
+	static int index = 0;
 	
+	public static void main(String args[]) throws IOException {
 	    System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+	    
+	    // file directory of images
+	    File dir = new File("../Interpolation/src/images");
+	    
+	    if(dir.exists()) {
+	    	for (File f : dir.listFiles()) {
+	    		addMat(f);
+	    	}
+	    }else {
+	    	System.out.println("File does not exist");
+	    	System.exit(0);
+	    }
+	    
+	    listener();
 
-	    Mat src = Imgcodecs.imread("/home/corwin/samples/animals/camel.jpeg");
+	    Mat src = mats.get(0);
+	    double scale = 400.0;
 	    
-	    BufferedImage buf1 = interpolation(src, Imgproc.INTER_NEAREST);
-	    BufferedImage buf2 = interpolation(src, Imgproc.INTER_LINEAR);
-	    BufferedImage buf3 = interpolation(src, Imgproc.INTER_LINEAR_EXACT);
-	    BufferedImage buf4 = interpolation(src, Imgproc.INTER_CUBIC);
+	    // scale to fit
+	    interpolation(src, Imgproc.INTER_NEAREST , scale/src.rows());
+	    labelText.add("Nearest Neighbor las-vegas-1");
+	    interpolation(src, Imgproc.INTER_LINEAR , scale/src.rows());
+	    labelText.add("Bilinear las-vegas-1");
+	    interpolation(src, Imgproc.INTER_CUBIC , scale/src.rows());
+	    labelText.add("Bicubic las-vegas-1");
 	    
-	    ImageIcon icon1 = new ImageIcon(buf1);
-	    ImageIcon icon2 = new ImageIcon(buf2);
-	    ImageIcon icon3 = new ImageIcon(buf3);
-	    ImageIcon icon4 = new ImageIcon(buf4);
-	    JLabel label1 = new JLabel(icon1);
-	    JLabel label2 = new JLabel(icon2);
-	    JLabel label3 = new JLabel(icon3);
-	    JLabel label4 = new JLabel(icon4);
-	    JFrame frame = new JFrame();
-	    JPanel panel = new JPanel();
-	    JPanel panel2 = new JPanel();
-	    GridLayout grid = new GridLayout(2,2);
-	    label1.setHorizontalTextPosition(JLabel.CENTER);
-	    label1.setVerticalTextPosition(JLabel.BOTTOM);
-	    label2.setHorizontalTextPosition(JLabel.CENTER);
-	    label2.setVerticalTextPosition(JLabel.BOTTOM);
-	    label3.setHorizontalTextPosition(JLabel.CENTER);
-	    label3.setVerticalTextPosition(JLabel.BOTTOM);
-	    label4.setHorizontalTextPosition(JLabel.CENTER);
-	    label4.setVerticalTextPosition(JLabel.BOTTOM);
+	    src = mats.get(1);
 	    
-	   
-	    panel.add(label1);
-	    panel.add(label2);
-	    panel2.add(label3);
-	    panel2.add(label4);
-	    label1.setText("Nearest Neighbor");
-	    label2.setText("Linear");
-	    label3.setText("Bilinear");
-	    label4.setText("Bicubic");
-	    frame.setLayout(grid);
-		frame.getContentPane().add(panel);
-		frame.getContentPane().add(panel2);
-		frame.pack();
-		frame.setVisible(true);
+	    // scale to fit
+	    interpolation(src, Imgproc.INTER_NEAREST , scale/src.rows());
+	    labelText.add("Nearest Neighbor las-vegas-2");
+	    interpolation(src, Imgproc.INTER_LINEAR , scale/src.rows());
+	    labelText.add("Bilinear las-vegas-2");
+	    interpolation(src, Imgproc.INTER_CUBIC , scale/src.rows());
+	    labelText.add("Bicubic las-vegas-2");
+	    
+	    src = mats.get(2);
+	    
+	    // scale to fit
+	    interpolation(src, Imgproc.INTER_NEAREST , scale/src.rows());
+	    labelText.add("Nearest Neighbor las-vegas-3");
+	    interpolation(src, Imgproc.INTER_LINEAR , scale/src.rows());
+	    labelText.add("Bilinear las-vegas-3");
+	    interpolation(src, Imgproc.INTER_CUBIC , scale/src.rows());
+	    labelText.add("Bicubic las-vegas-3");
+	    
+	    showFrame();
+	  	    
 	   }
 	
-	public static BufferedImage interpolation(Mat src, int inter) {
+	public static void addMat(File f) {
+		Mat img = Imgcodecs.imread(f.toString());
+		mats.add(img);
+	}
+	
+	public static void interpolation(Mat src, int inter, double ratio) {
 		
 		Mat dst = new Mat();
 
-	    Size size = new Size(src.cols()*1.5, src.rows()*1.5);
+	    Size size = new Size(src.cols()*ratio, src.rows()*ratio);
 
 	    Imgproc.resize(src, dst, size, 0, 0, inter);
 	    
 	    BufferedImage bufImage = null;
 		
 		MatOfByte matOfByte = new MatOfByte();       
-	    Imgcodecs.imencode(".jpeg", dst, matOfByte);
+	    Imgcodecs.imencode(".jpg", dst, matOfByte);
 
 	    byte[] byteArray = matOfByte.toArray(); 
 
@@ -88,7 +105,74 @@ public class Interpolation {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}  
+		images.add(bufImage);
+	}
+	
+	/**
+	 * adds key listener to frame.
+	 * n/space - next image
+	 * p - previous image
+	 * q - exit program
+	 */
+	public static void listener() {
+		frame.addKeyListener(new KeyListener(){
+
+			@Override
+			public void keyPressed(KeyEvent a) {
+				if(a.getKeyChar() == 'n' || a.getKeyCode() == KeyEvent.VK_SPACE) {
+					nextImage();
+				}
+				
+				if(a.getKeyChar() == 'p') {
+					previousImage();
+				}
+				
+				if(a.getKeyChar() == 'q') {
+					System.exit(0);
+				}	
+			}
+
+			@Override
+			public void keyReleased(KeyEvent a) {}
+			@Override
+			public void keyTyped(KeyEvent a) {}
+		});
+	}
+	
+	/**
+	 * sets index to next within bounds value
+	 */
+	public static void nextImage() {
+		if (index < images.size() - 1) {
+			index++;
+		} else {
+			index = 0;
 		}
-		return bufImage;
+		showFrame();
+	}
+	
+	/**
+	 * sets index to previous within bounds value
+	 */
+	public static void previousImage() {
+		if (index == 0) {
+			index = images.size() - 1;
+		} else {
+			index--;
+		}
+		showFrame();
+	}
+	
+	public static void showFrame() {
+		BufferedImage buf = images.get(index);
+		ImageIcon icon = new ImageIcon(buf);
+	    JLabel label = new JLabel(icon);
+
+	    frame.add(label);
+		frame.getContentPane();
+		frame.pack();
+		frame.setVisible(true);
+		System.out.println(index + " " + labelText.get(index));
 	}
 }
